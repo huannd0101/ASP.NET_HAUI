908e40dc0c78c45df4c4ea373a77d2c98c526bd8
Only Huân <71635685+huannd0101@users.noreply.github.com>
2021-11-10 21:01:51 +0700

    Ktra thường xuyên 1

---
+++ GiaiBaiKtra/NguyenDinhHuan_2019602287_De3/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NguyenDinhHuan_2019602287_De3.Models;
+
+namespace NguyenDinhHuan_2019602287_De3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Cau1()
+        {
+            return View();
+        }
+
+        public ActionResult Cau2()
+        {
+            return View();
+        }
+
+        public ActionResult KetQua(NhanVien nv)
+        {
+            if(nv.SoNgay >= 25)
+            {
+                nv.ThuNhap = (nv.LuongNgay * nv.SoNgay) + 500000;
+            }else
+            {
+                nv.ThuNhap = (nv.LuongNgay * nv.SoNgay);
+            }
+            return View(nv);
+        }
+    }
+}
---
+++ GiaiBaiKtra/NguyenDinhHuan_2019602287_De3/Models/NhanVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NguyenDinhHuan_2019602287_De3.Models
+{
+    public class NhanVien
+    {
+        public string MaNV { get; set; }
+        public string HoTen { get; set; }
+        public string DonVi { get; set; }
+        public int LuongNgay { get; set; }
+        public int SoNgay { get; set; }
+        public int ThuNhap { get; set; }
+    }
+}
